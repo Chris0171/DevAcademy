5ee19043e374fde04748613eb70dbf3e5f468702
Christian Milanes <christianmilanes01@gmail.com>
2026-03-04 19:05:13 +0100

    updated

--- 01-C++/Bucles/main.cpp
+++ 01-C++/Bucles/main.cpp
@@ -13,12 +13,17 @@ void imprimirPar(int a)
 
 int main()
 {
-    int arr[] = {1, 2, 5, 5, 9, 6};
+    /* int arr[] = {1, 2, 5, 5, 9, 6};
 
     for (auto i : arr)
     {
         imprimirPar(i);
-    }
+    } */
+   double i = 1.123456789;
+   float j = 1.123456789;
+   cout.precision(10);
+   cout << i << endl;
+   cout << j << endl;
 
     return 0;
 }
