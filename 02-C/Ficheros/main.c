08c438d13ab87b68a5657ef1461f639f8712c0b4
Christian Milanes <christianmilanes01@gmail.com>
2026-03-13 13:41:51 +0100

    updated

--- 02-C/Ficheros/main.c
+++ 02-C/Ficheros/main.c
@@ -9,7 +9,7 @@ struct Alumno
     
 };
 
-// Funciones void || funciones con retorno 
+// Crear ficher de texto 
 void crearFicheroTexto(){
     FILE *fichero;
 
@@ -36,6 +36,9 @@ void crearFicheroTexto(){
         printf ("Nota: ");
         scanf("%f", &al.nota);
 
+        // limpiar el buffer de entrada
+        while(getchar() != '\n');
+
 
         fprintf(fichero, "%d %s %.2f\n",
             al.id, al.nombre, al.nota);
@@ -45,16 +48,122 @@ void crearFicheroTexto(){
     fclose(fichero);
 }
 
+// Leer fichero de texto
+void leerFicheroTexto(){
+    // Se quiere leer el fichero y mostrar algunos datos
+    FILE *archivoTexto;
+    struct Alumno al;
+
+    archivoTexto = fopen("alumnos.txt", "r");
+    
+    if(archivoTexto == NULL){
+        printf("Error al abrir el fichero");
+        return; 
+    }
+    // El while funciona mientras se puendan leer 3 elementos, 
+    // Si no se puede leer 3 elementos, se sale del bulcle
+    while(fscanf(archivoTexto, "%d %s %f", &al.id, al.nombre, &al.nota) == 3){
+        printf("ID: %d\n", al.id);
+        printf("Nombre: %s\n", al.nombre);
+        printf("Nota: %.2f\n", al.nota);
+    }
+    fclose(archivoTexto);
+
+}
+
+// Convertir fichero de texto a binario
+void convertirTextoABinario(){
+    FILE *file;
+    FILE *binaryFile;
+
+    struct Alumno alumno;
+
+    file = fopen("alumnos.txt", "r");
+    binaryFile = fopen("alumnos.dat", "wb");
+
+    if(file == NULL || binaryFile == NULL){
+        printf("Error al abrir el fichero\n");
+        return;
+    }
+
+    while(fscanf(file, "%d %s %f", &alumno.id,
+         alumno.nombre, &alumno.nota) == 3){
+        
+        // Escribir una estructura completa en el fichero binario
+        // Un fichero binario guarda sus datos tal cual se encuentran
+        // en memoria. 
+        fwrite(&alumno, sizeof(struct Alumno), 1, binaryFile);
+    }
+    fclose(file);
+    fclose(binaryFile);
+}
+
+// Modificar nota usando posicionamiento
+void modificarNota(){
+    FILE *bFile;
+    struct Alumno al;
+    int idAl;
+
+    bFile = fopen("alumnos.dat", "rb+");
+
+    if(bFile == NULL){
+        printf("Error al abrir el fichero\n");
+        return;
+    }
+
+    printf("ID del alumno: ");
+    scanf("%d", &idAl);
+
+    while(fread(&al, sizeof(struct Alumno), 1, bFile) == 1){
+        if(al.id == idAl){
+            printf("Nueva nota: ");
+            float nuevaNota;
+            scanf("%f", &nuevaNota);
+
+            al.nota = nuevaNota;
+
+            fseek(bFile, -sizeof(struct Alumno), SEEK_CUR);
+
+            fwrite(&al, sizeof(struct Alumno), 1, bFile);
+
+            printf("Nota modificada correctamente\n");
+
+            fclose(bFile);
+            return;
+        }
+    }
+
+    printf("Alumno no encontrado\n");
+    fclose(bFile);
+}
+
+char* renombrar(char *nombreActual, char *nuevoNombre){
+    // true -> 0 false -> -1
+    if(rename(nombreActual, nuevoNombre) == 0){
+        printf("Fichero renombrado correctamente\n");
+        return nuevoNombre;
+    } else {
+        printf("Error al renombrar el fichero\n");
+        return nombreActual;
+    }
+}
+void borrar(char *nombreFichero){
+    if(remove(nombreFichero) == 0){
+        printf("Fichero borrado correctamente\n");
+    } else {
+        printf("Error al borrar el fichero\n");
+    }
+}
+
 
 int main(){
-    // struct Alumno al1 = {1, "Juan Rodriguez", 7.3};
-    // struct Alumno al2 = {2, "Maria Gomez", 8.5};
-    // struct Alumno al3; 
+    char *nombreFichero = "alumnos.txt";
 
-    // al3.nota = 9.0;
-    // al3.id = 3; 
+    nombreFichero = renombrar(nombreFichero, "al_renamed.txt");
 
-    crearFicheroTexto();
+    printf("Nombre del fichero: %s\n", nombreFichero);
+    
+    borrar(nombreFichero);
 
     return 0;
 }
